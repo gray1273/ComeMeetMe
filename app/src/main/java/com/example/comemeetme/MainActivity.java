404908863a9d68ad7f8218ac6a5292ec2dbaa602
maxg66 <57154880+maxg66@users.noreply.github.com>
2021-10-06 16:04:59 -0400

    Added sign in page activity and example fragment

--- app/src/main/java/com/example/comemeetme/MainActivity.java
+++ app/src/main/java/com/example/comemeetme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.comemeetme;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .add(R.id.fragment_container_view, BlankFragment.class, null)
+                        .commit();
+            }
+        });
     }
 }
