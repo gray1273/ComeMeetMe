5499e8397f666997a8b024e6c300012f84d9abfe
Nathan Gray <gray.1273@buckeyemail.osu.edu>
2021-10-22 00:06:21 -0400

    Added code to test the database

--- app/src/main/java/com/example/comemeetme/MainActivity.java
+++ app/src/main/java/com/example/comemeetme/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.comemeetme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
+                //Enter information to send to the database;
+                FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference mDbRef = mDatabase.getReference("Donor/Name");
+
+                mDbRef.setValue("Test Test").addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, e.getLocalizedMessage());
+                    }
+                });
+
                 getSupportFragmentManager().beginTransaction()
                         .setReorderingAllowed(true)
                         .add(R.id.fragment_container_view, BlankFragment.class, null)
